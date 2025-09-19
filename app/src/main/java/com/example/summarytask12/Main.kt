2d16364b306948d0b46d8ab45d29ed8731302b9a
lehuong0150 <lehuong1050@gmail.com>
2025-09-19 17:02:57 +0700

    push code 19-09

--- app/src/main/java/com/example/summarytask12/Main.kt
+++ app/src/main/java/com/example/summarytask12/Main.kt
@@ -4,8 +4,6 @@ import com.example.summarytask12.database.HotelDB
 
 fun main() {
 
-//    println(HotelDB.getCustomerList())
-//    println(HotelDB.getRoomList())
     println("-----------------------------------------------------------------------------------------")
     val app = HotelBookingApp()
     app.showMenu()
